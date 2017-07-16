914048327dd76a6da0bb6e51f16f285dc29e60c3
betsolfreshers2017 <betsolfreshers2017@gmail.com>
2017-07-16 23:23:52 +0530

    deploy this code

--- src/TestProject.java
+++ src/TestProject.java
@@ -3,7 +3,6 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//This is just a hello world project
 		System.out.println("Hello World");
 	}
 
